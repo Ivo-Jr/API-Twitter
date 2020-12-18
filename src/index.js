5409a0439314a53946634736ba0c3b687ff39b3a
Ivo Junior <maciel.jr95@gmail.com>
2020-12-18 14:01:31 -0300

    dados tratados

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import express from 'express';
 import Twit from 'twit'; 
-// import dotenv from 'dotenv';
-// dotenv.config();
 require('dotenv').config()
-
 
 const app = express();
 
 app.use(express.json());
-
-let name = "";
-let profileImage = "";
-let text = "";
 
 
 var T = new Twit({
@@ -21,26 +14,67 @@ var T = new Twit({
         access_token_secret: process.env.ACCESS_T_S,
         timeout_ms: 60 * 1000,
         strictSSL: true,
-})
-    
-
-
-
-app.get('/teste', (request, response) => {
-    // console.log(process.env)
-    T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TechTudo&count=1', (err, data, response) => {
-        for( let dados of data)(
-            console.log(dados)
-        )
-    })
-    // T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=updateordienet&count=1', (err, data, response) => {
-    //     for( let dados of data)(
-    //         console.log(dados)
-    //     )
-    // })
-    
-    return response.sendStatus(200)
 });
+    
+
+app.get('/teste', async (request, response) => {
+
+    var dt = {};
+    var dt2 = {};
+
+
+    T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TechTudo&count=1', (err, data, response) => {
+
+        var obj = data[0];
+        dt = {
+            profileImage: obj.user.profile_image_url,
+            name: obj.user.name,
+            texto: obj.text,
+        };
+    });
+
+    T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=updateordienet&count=1', (err, data, response) => {
+    
+        var obj = data[0];
+        dt2 = {
+            profileImage2: obj.user.profile_image_url,
+            name2: obj.user.name,
+            texto2: obj.text,
+        };
+    });
+        
+    setTimeout(() => {
+        return response.json([dt, dt2]);
+    }, 1000);
+    
+});
+
+        // data.map(item => {
+        //     profileImage = item.user.profile_image_url;
+        //     name = item.user.name;
+        //     texto = item.text;
+        // });
+
+        // temp['name'] = name;
+        // temp['texto'] = texto;
+        // temp['profileImage'] = profileImage;
+        // });
+  
+    
+    // T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=updateordienet&count=1', (err, data, response) => {
+    
+    //     data.map(item => {
+    //         name2 = item.user.name;
+    //         texto2 = item.text;
+    //         profileImage2 = item.user.profile_image_url;
+    //     });
+        
+    //     // arr.push({name2, texto2, profileImage2});
+    // });    
+    
+
+    
+    // return response.json([temp]);
 
 
 app.listen(8000, () => {

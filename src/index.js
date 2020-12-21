f7c34a255b2edade0e3637252ca0912d9066ae83
Ivo Junior <maciel.jr95@gmail.com>
2020-12-21 17:11:30 -0300

    commitado

--- src/index.js
+++ src/index.js
@@ -17,64 +17,48 @@ var T = new Twit({
 });
     
 
-app.get('/teste', async (request, response) => {
+    app.get('/teste', async (request, response) => {
 
-    var dt = {};
-    var dt2 = {};
-
+    let obj = {};
+    let obj2 = {};
 
     T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TechTudo&count=1', (err, data, response) => {
 
-        var obj = data[0];
-        dt = {
-            profileImage: obj.user.profile_image_url,
-            name: obj.user.name,
-            texto: obj.text,
-        };
+        data.map(item => {
+            obj.name = item.user.name;
+            obj.texto = item.text;
+            obj.profileImage = item.user.profile_image_url;
+        });
+
+        // var obj = data[0];
+        // dt = {
+        //     profileImage: obj.user.profile_image_url,
+        //     name: obj.user.name,
+        //     texto: obj.text,
+        // };
     });
 
-    T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=updateordienet&count=1', (err, data, response) => {
-    
-        var obj = data[0];
-        dt2 = {
-            profileImage2: obj.user.profile_image_url,
-            name2: obj.user.name,
-            texto2: obj.text,
-        };
-    });
-        
-    setTimeout(() => {
-        return response.json([dt, dt2]);
-    }, 1000);
-    
-});
-
-        // data.map(item => {
-        //     profileImage = item.user.profile_image_url;
-        //     name = item.user.name;
-        //     texto = item.text;
-        // });
-
-        // temp['name'] = name;
-        // temp['texto'] = texto;
-        // temp['profileImage'] = profileImage;
-        // });
-  
-    
     // T.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=updateordienet&count=1', (err, data, response) => {
     
     //     data.map(item => {
-    //         name2 = item.user.name;
-    //         texto2 = item.text;
-    //         profileImage2 = item.user.profile_image_url;
-    //     });
-        
-    //     // arr.push({name2, texto2, profileImage2});
-    // });    
-    
+    //         obj2.name2 = item.user.name;
+    //         obj2.texto2 = item.text;
+    //         obj2.profileImage2 = item.user.profile_image_url;
+    //     })
 
+    //     // var obj = data[0];
+    //     // dt2 = {
+    //     //     profileImage2: obj.user.profile_image_url,
+    //     //     name2: obj.user.name,
+    //     //     texto2: obj.text,
+    //     // };
+    // });
+        
+    setTimeout(() => {
+        return response.json([obj, obj2]);
+    }, 1000);
     
-    // return response.json([temp]);
+});
 
 
 app.listen(8000, () => {
